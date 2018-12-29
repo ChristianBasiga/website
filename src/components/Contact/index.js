31ec3198b50d76cf5ddb869dce63b3bd62814622
Prince Christian Basiga <princebasiga@gmail.com>
2018-12-28 23:53:11 -0800

    Implemented contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,55 @@
 import styled from 'styled-components';
 
 
-const ContactWrapper = styled.div`
+ const PageWrapper = styled.div`
+
+    width:40%;
+    display:grid;
+    margin:auto;
+    grid-template-columns: auto;
+    grid-template-rows: auto auto auto auto 20px;
+    grid-template-areas:
+    "contact-header"
+    "contacts"
+    "resume-header"
+    "resume"
+    ". ";
+
 
 
 `;
 
-export default ContactWrapper;
+//To reduce duplicate will set grid-area in style inline.
+export const Header = styled.p`
+
+    font-weight: bold;
+    color:white;
+    font-size:20px;
+`;
+
+export const ContactWrapper = styled.div`
+
+    display:flex;
+    width:100%;
+    justify-content:space-evenly;
+    
+
+`;
+
+export const ContactLink = styled.a`
+
+
+    background-image: url(${ props => props.image});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    cursor: pointer;
+    width: 50px;
+    height:50px;
+    
+
+`;
+
+
+
+export default PageWrapper;
--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,18 +1,38 @@
-import React, {Component } from 'react';
+import React from 'react';
 
 
-import ContactWrapper  from '../../components/Contact';
+import PageWrapper, {ContactLink, ContactWrapper, Header}  from '../../components/Contact';
 
 
 
-class Contact extends Component{
+const Contact = (props) => {
+
+    const resumeURL = "https://firebasestorage.googleapis.com/v0/b/chrisbtreat-website.appspot.com/o/Prince%20Christian%20Basiga%20Resume.pdf?alt=media&token=2b212b1c-b5cf-4396-95d8-3493904715fd";
+
+    console.log("contact props", props);
+    return (
+    
+        <PageWrapper>
+    <Header style = {{gridName: "contact-header"}}> Get in Touch!</Header>
+
+    <ContactWrapper style = {{gridName: "contacts"}}>
+
+            {props.contactLinks.map( contact => {
+                    console.log(contact);
+                    return <ContactLink  key = {contact.url} href = {contact.url} image={contact.image} alt={contact.title}/>
+
+            })}
+
+    </ContactWrapper>
+
+    <Header style = {{gridName: "resume-header"}}>Download my Resume </Header>
+    <ContactWrapper style = {{gridName: "resume"}}>
+    <ContactLink key="resume" href = {resumeURL} image = {require("../../images/pdfIcon.png")}/>
+    
+    </ContactWrapper>
 
 
-    render(){
-
-        return (<ContactWrapper> </ContactWrapper>);
-    }
-
+    </PageWrapper>)
 
 
 }
