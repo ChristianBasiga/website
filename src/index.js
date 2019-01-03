ab06bfba50068b2149642a4d25437187e6d6dd21
Prince Christian Basiga <princebasiga@gmail.com>
2019-01-02 23:54:19 -0800

    Set up firebase config via env variables

--- src/index.js
+++ src/index.js
@@ -5,7 +5,28 @@ import App from './containers/App';
 import {Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import createBrowserHistory from "history/createBrowserHistory";
+import firebase from 'firebase';
+require('dotenv/config');
 
+
+console.log("env variables", process.env);
+
+const config = {
+
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+}
+
+
+firebase.initializeApp(config);
+
+const settings = { timestampsInSnapshots: true};
+firebase.firestore().settings(settings);
+  
 const history =  createBrowserHistory();
 ReactDOM.render(<Router history = {history}><App /></Router>, document.getElementById('root'));
 
